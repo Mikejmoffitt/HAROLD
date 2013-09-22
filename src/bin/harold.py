89b1f1e5d204f68a7ef7781bc6dbaa810f0dc5bc
Michael Moffitt <mikejmoffitt@gmail.com>
2013-09-22 19:57:54 -0400

    Important update to the playing function
    200 seconds for me, y / y

--- src/bin/harold.py
+++ src/bin/harold.py
@@ -22,21 +22,24 @@ while 1:
 	id = ser.readline()
 	id = id[1:]
 	id = id.rstrip()
-	print "ibutton id read: " + id
+	print "iButton ID read: " + id
 
 	# TODO: check ID CRC, skip rest of loop if bad
 	
+	# Some Russ fun to link iButton IDs to ldap usernames
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect(("totoro.csh.rit.edu", 56123))
 	s.send(id + "\n");
 	
-	print "sent data to totoro..."
+	print "Sent data to totoro..."
 
-	time.sleep(1)
+	time.sleep(0.4)
+	
+	# better not have any usernames over 100 characters!
 	username = s.recv(100)
 	username = username.rstrip()
 
-	print "recvd username: " + username
+	print "Received username: " + username
 	if len(username) < 1:
 		print "Invalid iButton ID"
 		continue
@@ -69,6 +72,8 @@ while 1:
 	print "executing command: " + "mplayer \"/var/www/songs/" + username + "/" + filename + "\" "
 	if username == "astebbin":
 		os.system("mplayer -endpos 30 \"/var/www/songs/" + username + "/" + filename + "\" ");
+	elif username == "moffitt":
+		os.system("mplayer -endpos 200 \"/var/www/songs/" + username  "/" + filename + "\" ");
 	else:
-		os.system("mplayer -endpos 20 \"/var/www/songs/" + username + "/" + filename + "\" ");
+		os.system("mplayer -endpos 25 \"/var/www/songs/" + username + "/" + filename + "\" ");
 	ser.flushInput()
